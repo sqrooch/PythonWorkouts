bf5ee39c97805e1ff3b356d6747fa28fed405153
sqrooch <sqrooch@yandex.ru>
2022-10-12 18:43:48 +0300

    Закончил Часть III.

---
+++ algorithms/from_decimal_to_any.py
@@ -0,0 +1,6 @@
+number = int(input('Enter a number: '))
+base = int(input('Enter a base of numeral system: '))
+
+while number > 0:
+    print(number % base, end='')
+    number //= base
---
+++ lutz/learning_python/exercises/part3/task2/task2.py
@@ -0,0 +1,5 @@
+import winsound
+
+for i in range(10):
+    print('Hello %d\n' % i)
+    winsound.Beep(600, 400)
