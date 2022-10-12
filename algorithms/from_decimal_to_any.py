f7ad5714b7b02812c7b67efac801538b5019d1d1
sqrooch <sqrooch@yandex.ru>
2022-10-12 18:48:17 +0300

    Сменил название директория на "algorithms".

--- algorithms/from_decimal_to_any.py
+++ algorithms/from_decimal_to_any.py
@@ -4,4 +4,3 @@ base = int(input('Enter a base of numeral system: '))
 while number > 0:
     print(number % base, end='')
     number //= base
-
--- algorithms/sieve_of_eratosthenes.py
+++ algorithms/sieve_of_eratosthenes.py
@@ -9,18 +9,18 @@ def sieve_of_eratosthenes(n):
     An array of prime numbers will also be printed to the console screen.
     The argument n is a natural integer that is the upper limit of the number range under consideration."""
 
-    primes_highlighter = [True] * (n+1)
+    primes_highlighter = [True] * (n + 1)
     primes_highlighter[0] = primes_highlighter[1] = False
     primes_counter = 0
 
-    for i in range(2, n+1):
+    for i in range(2, n + 1):
         if primes_highlighter[i]:
             primes_counter += 1
-            for j in range(2*i, n+1, i):
+            for j in range(2 * i, n + 1, i):
                 primes_highlighter[j] = False
 
     print("\nYou have the following array of primes in your chosen range: ")
-    for i in range(n+1):
+    for i in range(n + 1):
         if primes_highlighter[i]:
             print(i, end=" ")
 
@@ -29,4 +29,3 @@ def sieve_of_eratosthenes(n):
 
 userInput = int(input("\nEnter here the upper limit of the range to find the number of all primes in it: "))
 print(f"\nThere are {sieve_of_eratosthenes(userInput)} primes in the range you suggested.")
-
